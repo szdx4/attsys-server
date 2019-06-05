9063cf81f52fc72559871c855b3ce15e6b51a00b
DingZhenyuan <ding_zy@outlook.com>
2019-06-05 18:41:00 +0800

    加入添加排班功能

--- controllers/shift.go
+++ controllers/shift.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/szdx4/attsys-server/models"
 	"github.com/szdx4/attsys-server/requests"
 	"github.com/szdx4/attsys-server/utils/database"
 	"github.com/szdx4/attsys-server/utils/response"
+	"strconv"
+	"time"
 )
 
 // ShiftCreate 添加排班
@@ -16,18 +19,37 @@ func ShiftCreate(c *gin.Context) {
 		c.Abort()
 		return
 	}
-	if err := req.Validate(); err != nil {
+
+	if err := req.Validate(c); err != nil {
 		response.BadRequest(c, err.Error())
 		c.Abort()
 		return
 	}
 
+	// 构造并存入数据库
+	var timeLayOut = "2006-01-02 15:04:05"
+	startAt, err := time.Parse(timeLayOut, req.StartAt)
+	if err != nil {
+		response.BadRequest(c, errors.New("start_at not valid").Error())
+		c.Abort()
+		return
+	}
+	endAt, err := time.Parse(timeLayOut, req.EndAt)
+	if err != nil {
+		response.BadRequest(c, errors.New("end_at not valid").Error())
+		c.Abort()
+		return
+	}
+
+	userID, _ := strconv.Atoi(c.Param("id"))
 	shift := models.Shift{
-		StartAt: req.StartAt,
-		EndAt:   req.EndAt,
+		UserID:  uint(userID),
+		StartAt: startAt,
+		EndAt:   endAt,
 		Type:    req.Type,
 		Status:  "no",
 	}
+
 	database.Connector.Create(&shift)
 	if shift.ID < 1 {
 		response.InternalServerError(c, "Internal Server Error")
--- requests/shift.go
+++ requests/shift.go
@@ -2,26 +2,51 @@ package requests
 
 import (
 	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/szdx4/attsys-server/models"
+	"github.com/szdx4/attsys-server/utils/database"
+	"strconv"
+
 	"time"
 )
 
 // ShiftCreateRequest 添加排班
 type ShiftCreateRequest struct {
-	StartAt time.Time `binding:"required"`
-	EndAt   time.Time `binding:"required"`
-	Type    string    `binding:"required"`
+	StartAt string `json:"start_at"`
+	EndAt   string `json:"end_at"`
+	Type    string `binding:"required"`
 }
 
 // Validate 验证 ShiftCreateRequest 请求中的有效性
-func (r *ShiftCreateRequest) Validate() error {
-	// 验证开始时间的有效性
-	//if len(r.StartAt) != 9 {
-	//	return errors.New("Start time not valid")
-	//}
-	//// 验证结束时间的有效性
-	//if len(r.EndAt) != 9 {
-	//	return errors.New("End time not valid")
-	//}
+func (r *ShiftCreateRequest) Validate(c *gin.Context) error {
+	//将接收的 string 格式转换成 Time
+	var timeLayOut = "2006-01-02 15:04:05"
+	startAt, err := time.Parse(timeLayOut, r.StartAt)
+	if err != nil {
+		return errors.New("start_at not valid")
+	}
+	endAt, err := time.Parse(timeLayOut, r.EndAt)
+	if err != nil {
+		return errors.New("end_at not valid")
+	}
+
+	// 验证给出排班的有效性
+	if startAt.After(endAt) {
+		return errors.New("Time not valid")
+	}
+
+	// 验证个人排班的冲突性
+	shifts := []models.Shift{}
+	userID, _ := strconv.Atoi(c.Param("id"))
+	db := database.Connector
+	db = db.Where("user_id = ?", userID)
+	db = db.Where("start_at <= ?", r.EndAt)
+	db = db.Where("end_at >= ?", r.StartAt)
+	db.Find(&shifts)
+	if len(shifts) != 0 {
+		return errors.New("Time is conflicting")
+	}
+
 	// 验证类型的有效性
 	if r.Type != "normal" && r.Type != "overtime" && r.Type != "allovertime" {
 		return errors.New("Type not valid")

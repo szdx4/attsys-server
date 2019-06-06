4e8aa7919fcd559e6ef04e0c081f4e259242a959
DingZhenyuan <ding_zy@outlook.com>
2019-06-06 08:47:52 +0800

    加入获取指定用户加班功能

--- utils/response/overtime.go
+++ utils/response/overtime.go
@@ -2,6 +2,8 @@ package response
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/szdx4/attsys-server/config"
+	"github.com/szdx4/attsys-server/models"
 	"net/http"
 )
 
@@ -10,5 +12,16 @@ func OvertimeCreate(c *gin.Context, overtimeID uint) {
 	c.JSON(http.StatusCreated, gin.H{
 		"status":   http.StatusCreated,
 		"shift_id": overtimeID,
+	})
+}
+
+// OvertimeShow 获取指定用户加班
+func OvertimeShow(c *gin.Context, total, page int, overtime []models.Overtime) {
+	c.JSON(http.StatusOK, gin.H{
+		"status":       http.StatusOK,
+		"total":        total,
+		"current_page": page,
+		"per_page":     config.App.ItemsPerPage,
+		"data":         overtime,
 	})
 }

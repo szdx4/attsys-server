4a45a02f564f1fc3a0f4d32097d28b01ea683fae
kotoyuuko <kokoro@core.moe>
2019-06-06 10:03:47 +0800

    二维码模型

---
+++ models/qrcode.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"encoding/base64"
+	"time"
+
+	"github.com/skip2/go-qrcode"
+)
+
+// Qrcode 二维码模型
+type Qrcode struct {
+	CommonFields
+	Token     string    `json:"token"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// Image 获取当前二维码的图片
+func (m *Qrcode) Image() (string, error) {
+	png, err := qrcode.Encode(m.Token, qrcode.Medium, 256)
+	if err != nil {
+		return "", err
+	}
+
+	base64 := base64.StdEncoding.EncodeToString(png)
+
+	return base64, nil
+}

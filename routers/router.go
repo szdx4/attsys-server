2739413f0a0dd94b7b9412539b9cd9b6ff23262f
kotoyuuko <kokoro@core.moe>
2019-06-09 15:28:02 +0800

    添加 CORS 头部

--- routers/router.go
+++ routers/router.go
@@ -1,11 +1,11 @@
 package routers
 
 import (
-	"github.com/szdx4/attsys-server/controllers"
-	"github.com/szdx4/attsys-server/middleware"
-
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/szdx4/attsys-server/config"
+	"github.com/szdx4/attsys-server/controllers"
+	"github.com/szdx4/attsys-server/middleware"
 )
 
 // Router 设置路由和公共中间件，返回 Gin Engine 对象
@@ -13,6 +13,11 @@ func Router() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"*"}
+	r.Use(cors.New(corsConfig))
+
 	gin.SetMode(config.App.RunMode)
 
 	r.GET("/", controllers.Home)

ddc4788b8247a3cb8806cfe5f253a528448aa8a0
kotoyuuko <kokoro@core.moe>
2019-06-09 09:34:38 +0800

    删除 departments 表中的 manager 字段

--- models/department.go
+++ models/department.go
@@ -3,7 +3,5 @@ package models
 // Department 部门模型
 type Department struct {
 	CommonFields
-	Name      string `json:"name"`
-	ManagerID uint   `json:"manager"`
-	Users     []User `json:"-" gorm:"foreignkey:DepartmentID"`
+	Name string `json:"name"`
 }

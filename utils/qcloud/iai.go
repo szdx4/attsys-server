be29390ce7de10931c641b78450d0cfb09391787
kotoyuuko <kokoro@core.moe>
2019-06-09 13:15:23 +0800

    删除人脸比对功能

--- utils/qcloud/iai.go
+++ utils/qcloud/iai.go
@@ -4,7 +4,6 @@ import (
 	goerrors "errors"
 	"strings"
 
-	"github.com/szdx4/attsys-server/config"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
@@ -129,40 +128,4 @@ func SearchFaces(groupID, image string) (string, error) {
 	}
 
 	return *candidates[0].PersonId, nil
-}
-
-// CompareFace 人脸比对
-func CompareFace(faceA, faceB string) (float64, error) {
-	if strings.Index(faceA, "data:") > -1 {
-		tmp := strings.Split(faceA, "base64,")
-		faceA = tmp[1]
-	}
-	if strings.Index(faceB, "data:") > -1 {
-		tmp := strings.Split(faceB, "base64,")
-		faceB = tmp[1]
-	}
-
-	credential := common.NewCredential(
-		config.Qcloud.SecretID,
-		config.Qcloud.SecretKey,
-	)
-
-	cpf := profile.NewClientProfile()
-	cpf.HttpProfile.ReqMethod = "POST"
-	cpf.HttpProfile.ReqTimeout = 10
-	cpf.HttpProfile.Endpoint = "iai.tencentcloudapi.com"
-	cpf.SignMethod = "TC3HmacSHA256"
-
-	client, _ := iai.NewClient(credential, regions.Beijing, cpf)
-
-	request := iai.NewCompareFaceRequest()
-	request.ImageA = common.StringPtr(faceA)
-	request.ImageB = common.StringPtr(faceB)
-
-	response, err := client.CompareFace(request)
-	if err != nil {
-		return 0.0, err
-	}
-
-	return *response.Response.Score, nil
 }

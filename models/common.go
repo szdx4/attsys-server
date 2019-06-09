de98b1dc055cce3b6bd52d3081c8ff8c7dd04361
kotoyuuko <kokoro@core.moe>
2019-06-09 14:15:24 +0800

    API 中去除 deleted_at

--- models/common.go
+++ models/common.go
@@ -7,5 +7,5 @@ type CommonFields struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
+	DeletedAt *time.Time `json:"-"`
 }

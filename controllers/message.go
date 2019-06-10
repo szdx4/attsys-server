ae7793b37cd44145680e9206949cb620393b06d4
kotoyuuko <kokoro@core.moe>
2019-06-10 10:38:14 +0800

    为 controllers/message.go 添加注释

--- controllers/message.go
+++ controllers/message.go
@@ -12,6 +12,7 @@ import (
 
 // MessageShow 获取指定信息
 func MessageShow(c *gin.Context) {
+	// 获取 URL 中的信息 ID
 	messageID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "Message ID invalid")
@@ -19,7 +20,7 @@ func MessageShow(c *gin.Context) {
 		return
 	}
 
-	//查找到信息
+	// 查找到信息
 	message := models.Message{}
 	database.Connector.Preload("FromUser").Preload("ToUser").First(&message, messageID)
 	if message.ID < 1 {
@@ -28,7 +29,7 @@ func MessageShow(c *gin.Context) {
 		return
 	}
 
-	// 只有发送人和接受者可以读取指定信息
+	// 只有发送人和接收人可以读取指定信息
 	authID, _ := c.Get("user_id")
 	if message.FromUserID != uint(authID.(int)) && message.ToUserID != uint(authID.(int)) {
 		response.Unauthorized(c, "This is not your message")
@@ -36,26 +37,28 @@ func MessageShow(c *gin.Context) {
 		return
 	}
 
-	// 如果是接受者读取指定信息，则变为已读
+	// 如果是接收人读取指定信息，则变为已读
 	if message.ToUserID == uint(authID.(int)) {
 		message.Status = "read"
+		database.Connector.Save(&message)
 	}
 
-	database.Connector.Save(&message)
-
+	// 发送响应
 	response.MessageShow(c, message)
 }
 
 // MessageList 获取信息列表
 func MessageList(c *gin.Context) {
+	// 初始化条件查询模型
 	messages := []models.Message{}
 	db := database.Connector.Preload("FromUser").Preload("ToUser").Order("created_at DESC")
 
+	// 获取认证用户信息
 	role, _ := c.Get("user_role")
 	authID, _ := c.Get("user_id")
 	flag := false
 
-	// 检测 from_user_id
+	// 检测 from_user_id 合法性
 	if fromUserID, isExist := c.GetQuery("from_user_id"); isExist {
 		fromUserID, _ := strconv.Atoi(fromUserID)
 		db = db.Where("from_user_id = ?", fromUserID)
@@ -64,7 +67,7 @@ func MessageList(c *gin.Context) {
 		}
 	}
 
-	// 检测 to_user_id
+	// 检测 to_user_id 合法性
 	if toUserID, isExist := c.GetQuery("to_user_id"); isExist {
 		toUserID, _ := strconv.Atoi(toUserID)
 		db = db.Where("to_user_id = ?", toUserID)
@@ -73,18 +76,19 @@ func MessageList(c *gin.Context) {
 		}
 	}
 
+	// 检测用户是否具有读取此类信息的权限
 	if !flag && role != "master" {
 		response.Unauthorized(c, "You are not authorized to get these messages")
 		c.Abort()
 		return
 	}
 
-	// 检测 status
+	// 检测 status 合法性
 	if status, isExist := c.GetQuery("status"); isExist {
 		db = db.Where("status = ?", status)
 	}
 
-	// 检测 page
+	// 处理分页
 	page, err := strconv.Atoi(c.Query("page"))
 	if err != nil {
 		page = 1
@@ -95,13 +99,17 @@ func MessageList(c *gin.Context) {
 	perPage := config.App.ItemsPerPage
 	total := 0
 
+	// 执行查询
 	db.Limit(perPage).Offset((page - 1) * perPage).Find(&messages)
 	db.Model(&models.Message{}).Count(&total)
+
+	// 判断当前页是否为空
 	if (page-1)*perPage >= total {
 		response.NoContent(c)
 		c.Abort()
 		return
 	}
 
+	// 发送响应
 	response.MessageList(c, total, page, messages)
 }

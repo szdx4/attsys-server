21139eeaec71b1136d8de96588669a2af924f5f2
kotoyuuko <kokoro@core.moe>
2019-06-10 13:30:50 +0800

    为 requests/sign.go 添加注释并删除人脸签到中对人脸存在性的检测

--- requests/sign.go
+++ requests/sign.go
@@ -19,13 +19,14 @@ type SignWithQrcodeRequest struct {
 
 // Validate 验证二维码签到请求的有效性
 func (r *SignWithQrcodeRequest) Validate() error {
+	// 验证二维码是否存在
 	qrcode := models.Qrcode{}
 	database.Connector.Where("token = ?", r.Token).First(&qrcode)
-
 	if qrcode.ID == 0 {
 		return errors.New("Qrcode not found")
 	}
 
+	// 验证二维码是否过期
 	if time.Now().After(qrcode.ExpiredAt) {
 		return errors.New("Qrcode expired")
 	}
@@ -40,13 +41,7 @@ type SignWithFaceRequest struct {
 
 // Validate 验证人脸签到请求的合法性
 func (r *SignWithFaceRequest) Validate(userID int) error {
-	face := models.Face{}
-	database.Connector.Where("user_id = ? AND status = 'available'", userID).First(&face)
-
-	if face.ID == 0 {
-		return errors.New("Face info not found")
-	}
-
+	// 验证人脸是否匹配
 	resID, err := qcloud.SearchFaces(config.Qcloud.GroupName, r.Face)
 	if err != nil {
 		return errors.New("Face not match")

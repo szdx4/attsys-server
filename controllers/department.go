2e3ebab32c17314ca409cb1afcc917e97c895164
kotoyuuko <kokoro@core.moe>
2019-06-10 10:03:15 +0800

    为 controllers/department.go 添加注释

--- controllers/department.go
+++ controllers/department.go
@@ -19,29 +19,34 @@ func DepartmentCreate(c *gin.Context) {
 		c.Abort()
 		return
 	}
-	// 检验
+
+	// 检验提交数据的合法性
 	if err := req.Validate(); err != nil {
 		response.BadRequest(c, err.Error())
 		c.Abort()
 		return
 	}
-	// 新建
+
+	// 创建新部门模型并保存到数据库
 	department := models.Department{
 		Name: req.Name,
 	}
 	database.Connector.Create(&department)
 
+	// 判断是否创建成功
 	if department.ID < 1 {
 		response.InternalServerError(c, "Database error")
 		c.Abort()
 		return
 	}
-	// 响应
+
+	// 发送响应
 	response.Created(c, department.ID)
 }
 
 // DepartmentList 获取部门列表
 func DepartmentList(c *gin.Context) {
+	// 处理分页
 	page, err := strconv.Atoi(c.Query("page"))
 	if err != nil {
 		page = 1
@@ -50,21 +55,27 @@ func DepartmentList(c *gin.Context) {
 		page = 1
 	}
 	perPage := config.App.ItemsPerPage
+
+	// 按分页查询对应的数据
 	departments := []models.Department{}
 	total := 0
 	database.Connector.Limit(perPage).Offset((page - 1) * perPage).Find(&departments)
 	database.Connector.Model(&models.Department{}).Count(&total)
 
+	// 判断当前页是否为空
 	if (page-1)*perPage >= total {
 		response.NoContent(c)
 		c.Abort()
 		return
 	}
+
+	// 发送响应
 	response.DepartmentList(c, total, page, departments)
 }
 
 // DepartmentShow 获取指定部门信息
 func DepartmentShow(c *gin.Context) {
+	// 从 URL 中获得部门 ID
 	departmentID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "Department ID invalid")
@@ -72,6 +83,7 @@ func DepartmentShow(c *gin.Context) {
 		return
 	}
 
+	// 数据库中查询部门
 	department := models.Department{}
 	database.Connector.First(&department, departmentID)
 	if department.ID == 0 {
@@ -80,6 +92,7 @@ func DepartmentShow(c *gin.Context) {
 		return
 	}
 
+	// 判断当前用户权限
 	role, _ := c.Get("user_role")
 	if role != "master" {
 		userID, _ := c.Get("user_id")
@@ -92,24 +105,27 @@ func DepartmentShow(c *gin.Context) {
 		}
 	}
 
+	// 发送响应
 	response.DepartmentShow(c, department)
 }
 
 // DepartmentUpdate 编辑部门
 func DepartmentUpdate(c *gin.Context) {
-	// 合法性检验
 	var req requests.DepartmentUpdateRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		response.BadRequest(c, err.Error())
 		c.Abort()
 		return
 	}
+
+	// 检验提交数据的合法性
 	if err := req.Validate(c); err != nil {
 		response.BadRequest(c, err.Error())
 		c.Abort()
 		return
 	}
 
+	// 从 URL 中获取部门 ID
 	departmentID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "Department ID invalid")
@@ -117,7 +133,7 @@ func DepartmentUpdate(c *gin.Context) {
 		return
 	}
 
-	// 从数据库中查找
+	// 从数据库中查找部门
 	department := models.Department{}
 	database.Connector.First(&department, departmentID)
 	if department.ID == 0 {
@@ -126,15 +142,17 @@ func DepartmentUpdate(c *gin.Context) {
 		return
 	}
 
-	// 编辑部门的相应信息
+	// 编辑部门的相应信息并保存
 	department.Name = req.Name
 	database.Connector.Save(&department)
 
+	// 发送响应
 	response.DepartmentUpdate(c)
 }
 
 // DepartmentDelete 删除部门
 func DepartmentDelete(c *gin.Context) {
+	// 从 URL 中获取部门 ID
 	departmentID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "Department ID invalid")
@@ -142,15 +160,20 @@ func DepartmentDelete(c *gin.Context) {
 		return
 	}
 
+	// 从数据库中查找部门
 	department := models.Department{}
 	database.Connector.First(&department, departmentID)
 
+	// 判断部门是否存在
 	if department.ID == 0 {
 		response.NotFound(c, "Department not found")
 		c.Abort()
 		return
 	}
+
+	// 执行删除操作
 	database.Connector.Delete(&department)
 
+	// 发送响应
 	response.DepartmentDelete(c)
 }

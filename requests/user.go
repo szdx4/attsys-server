0c4eef960da61709dae6f97e8df6011da1937510
kotoyuuko <kokoro@core.moe>
2019-06-10 13:33:16 +0800

    为 requests/user.go 添加注释

--- requests/user.go
+++ requests/user.go
@@ -22,12 +22,14 @@ type UserAuthRequest struct {
 
 // Validate 验证 UserAuthRequest 请求中用户信息的有效性
 func (r *UserAuthRequest) Validate() (*models.User, error) {
+	// 验证用户是否存在
 	user := &models.User{}
 	database.Connector.Where("name = ?", r.Name).First(&user)
 	if user.ID == 0 {
 		return nil, errors.New("User not found")
 	}
 
+	// 验证密码是否匹配
 	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(r.Password))
 	if err != nil {
 		return nil, errors.New("Password invalid")
@@ -45,20 +47,24 @@ type UserCreateRequest struct {
 
 // Validate 验证创建用户请求的合法性
 func (r *UserCreateRequest) Validate() error {
+	// 验证用户名长度
 	if len(r.Name) < config.App.MinUserLength {
 		return errors.New("User name must longer than " + strconv.Itoa(config.App.MinUserLength))
 	}
 
+	// 验证用户名存在性
 	user := models.User{}
 	database.Connector.Where("name = ?", r.Name).First(&user)
 	if user.ID > 0 {
 		return errors.New("User name exists")
 	}
 
+	// 验证密码长度
 	if len(r.Password) < config.App.MinPwdLength {
 		return errors.New("Password must longer than " + strconv.Itoa(config.App.MinPwdLength))
 	}
-	// 验证部门的存在与否
+
+	// 验证部门是否存在
 	department := models.Department{}
 	database.Connector.First(&department, r.Department)
 	if department.ID == 0 {
@@ -126,21 +132,24 @@ type UserPasswordRequest struct {
 
 // Validate 验证修改密码请求的合法性
 func (r *UserPasswordRequest) Validate(role string, authID, userID int) (string, error) {
+	// 用户必须输入原密码
 	if role == "user" && r.OldPassword == "" {
 		return "", errors.New("Old password missing")
 	}
 
+	// 用户只能修改自己的密码
 	if role == "user" && authID != userID {
 		return "", errors.New("You can only modify your own password")
 	}
 
+	// 验证用户是否存在
 	user := models.User{}
 	database.Connector.First(&user, userID)
-
 	if user.ID == 0 {
 		return "", errors.New("User not found")
 	}
 
+	// 验证用户的原密码
 	if role == "user" {
 		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(r.OldPassword))
 		if err != nil {
@@ -148,10 +157,12 @@ func (r *UserPasswordRequest) Validate(role string, authID, userID int) (string,
 		}
 	}
 
+	// 验证新密码的长度
 	if len(r.NewPassword) < config.App.MinPwdLength {
 		return "", errors.New("Password must longer than " + strconv.Itoa(config.App.MinPwdLength))
 	}
 
+	// 生成新密码的 bcrypt hash
 	hash, err := bcrypt.GenerateFromPassword([]byte(r.NewPassword), 10)
 	if err != nil {
 		return "", errors.New("Password hash generate error")

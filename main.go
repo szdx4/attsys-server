60dea1229bb1ddc6843cd120eeae3eba53f9ba3e
kotoyuuko <kokoro@core.moe>
2019-06-10 10:02:16 +0800

    为 main.go 添加注释

--- main.go
+++ main.go
@@ -14,15 +14,19 @@ import (
 )
 
 func main() {
+	// 初始化数据库和数据表
 	database.Connect()
 	database.Migrate()
 	database.Seed()
 
+	// 初始化腾讯云人脸识别库
 	qcloud.GroupInit()
 	qcloud.PersonInit()
 
+	// 初始化路由
 	router := routers.Router()
 
+	// 初始化 HTTP 服务器
 	server := &http.Server{
 		Addr:           fmt.Sprintf(":%d", config.Server.HTTPPort),
 		Handler:        router,
@@ -31,6 +35,7 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
+	// 启动 HTTP 服务器
 	log.Printf("Server listening at port: %d", config.Server.HTTPPort)
 	server.ListenAndServe()
 }

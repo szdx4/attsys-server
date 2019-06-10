0a9634aff165e685e5a295b48c840c6a7127a37b
kotoyuuko <kokoro@core.moe>
2019-06-10 13:24:51 +0800

    为 middleware/manager.go 添加注释

--- middleware/manager.go
+++ middleware/manager.go
@@ -7,6 +7,7 @@ import (
 
 // Manager 验证用户是否具有 manager 以上的权限
 func Manager(c *gin.Context) {
+	// 获取认证用户角色
 	role, ok := c.Get("user_role")
 	if !ok {
 		response.Unauthorized(c, "Access Denied")
@@ -14,6 +15,7 @@ func Manager(c *gin.Context) {
 		return
 	}
 
+	// 判断用户权限
 	if role != "manager" && role != "master" {
 		response.Unauthorized(c, "Access Denied")
 		c.Abort()

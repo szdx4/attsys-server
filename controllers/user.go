08ec709990b6085ee58260dffe4f01d2d10dec9f
kotoyuuko <kokoro@core.moe>
2019-06-10 13:22:05 +0800

    为 controllers/user.go 添加注释

--- controllers/user.go
+++ controllers/user.go
@@ -23,6 +23,7 @@ func UserAuth(c *gin.Context) {
 		return
 	}
 
+	// 验证提交数据的合法性
 	user, err := req.Validate()
 	if err != nil {
 		response.Unauthorized(c, err.Error())
@@ -30,12 +31,12 @@ func UserAuth(c *gin.Context) {
 		return
 	}
 
+	// 生成新的 JWT Token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"id":         user.ID,
 		"role":       user.Role,
 		"expired_at": time.Now().Add(time.Hour * time.Duration(config.App.TokenValid)).Format(time.RFC3339),
 	})
-
 	tokenString, err := token.SignedString([]byte(config.App.EncryptKey))
 	if err != nil {
 		response.InternalServerError(c, "Token sign error")
@@ -43,11 +44,13 @@ func UserAuth(c *gin.Context) {
 		return
 	}
 
+	// 发送响应
 	response.UserAuth(c, user.ID, tokenString)
 }
 
 // UserShow 获取单个用户信息
 func UserShow(c *gin.Context) {
+	// 获取 URL 中的用户 ID
 	userID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "User ID invalid")
@@ -55,8 +58,11 @@ func UserShow(c *gin.Context) {
 		return
 	}
 
+	// 获取认证用户信息
 	role, _ := c.Get("user_role")
 	authID, _ := c.Get("user_id")
+
+	// 查询用户信息
 	user := models.User{}
 	database.Connector.Preload("Department").First(&user, userID)
 	if user.ID < 1 {
@@ -65,12 +71,14 @@ func UserShow(c *gin.Context) {
 		return
 	}
 
+	// 用户只能获得自己的信息
 	if role == "user" && authID != userID {
 		response.Unauthorized(c, "You cannot get others information")
 		c.Abort()
 		return
 	}
 
+	// 部门主管只能获得本部门用户信息
 	if role == "manager" {
 		manager := models.User{}
 		database.Connector.First(&manager, authID)
@@ -81,6 +89,7 @@ func UserShow(c *gin.Context) {
 		}
 	}
 
+	// 发送响应
 	response.UserShow(c, user)
 }
 
@@ -93,12 +102,14 @@ func UserCreate(c *gin.Context) {
 		return
 	}
 
+	// 验证提交数据的合法性
 	if err := req.Validate(); err != nil {
 		response.BadRequest(c, err.Error())
 		c.Abort()
 		return
 	}
 
+	// 生成密码的 bcrypt hash
 	hash, err := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
 	if err != nil {
 		response.InternalServerError(c, "Password hash generate error")
@@ -106,6 +117,7 @@ func UserCreate(c *gin.Context) {
 		return
 	}
 
+	// 创建用户
 	user := models.User{
 		Name:         req.Name,
 		Password:     string(hash),
@@ -113,18 +125,19 @@ func UserCreate(c *gin.Context) {
 		Role:         "user",
 	}
 	database.Connector.Create(&user)
-
 	if user.ID < 1 {
 		response.InternalServerError(c, "Database error")
 		c.Abort()
 		return
 	}
 
+	// 发送响应
 	response.UserCreate(c, user.ID)
 }
 
 // UserList 获取用户列表
 func UserList(c *gin.Context) {
+	// 处理分页
 	page, err := strconv.Atoi(c.Query("page"))
 	if err != nil {
 		page = 1
@@ -137,8 +150,10 @@ func UserList(c *gin.Context) {
 	users := []models.User{}
 	total := 0
 
+	// 获取认证用户信息
 	role, _ := c.Get("user_role")
 
+	// 主管只能获得本部门用户列表
 	if role == "manager" {
 		managerID, _ := c.Get("user_id")
 		manager := models.User{}
@@ -154,17 +169,20 @@ func UserList(c *gin.Context) {
 		db.Model(&models.User{}).Count(&total)
 	}
 
+	// 判断当前页是否为空
 	if (page-1)*perPage >= total {
 		response.NoContent(c)
 		c.Abort()
 		return
 	}
 
+	// 发送响应
 	response.UserList(c, total, page, users)
 }
 
 // UserDelete 删除用户
 func UserDelete(c *gin.Context) {
+	// 获取 URL 中用户 ID
 	userID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "User ID invalid")
@@ -172,23 +190,24 @@ func UserDelete(c *gin.Context) {
 		return
 	}
 
+	// 查询用户
 	user := models.User{}
 	database.Connector.First(&user, userID)
-
 	if user.ID == 0 {
 		response.NotFound(c, "User not found")
 		c.Abort()
 		return
 	}
 
+	// 执行删除操作
 	database.Connector.Delete(&user)
 
+	// 发送响应
 	response.UserDelete(c, userID)
 }
 
 // UserUpdate 修改用户
 func UserUpdate(c *gin.Context) {
-	// 请求合法性检验
 	var req requests.UserUpdateRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		response.BadRequest(c, err.Error())
@@ -196,6 +215,7 @@ func UserUpdate(c *gin.Context) {
 		return
 	}
 
+	// 验证提交数据的合法性
 	userID, err := req.Validate(c)
 	if err != nil {
 		response.BadRequest(c, err.Error())
@@ -219,17 +239,17 @@ func UserUpdate(c *gin.Context) {
 	user.Hours = uint(req.Hours)
 	database.Connector.Save(&user)
 
-	department := models.Department{}
-	database.Connector.First(&department, user.DepartmentID)
-
+	// 发送响应
 	response.UserUpdate(c)
 }
 
 // UserPassword 修改密码
 func UserPassword(c *gin.Context) {
+	// 获取认证用户信息
 	role, _ := c.Get("user_role")
 	authID, _ := c.Get("user_id")
 
+	// 获取 URL 中用户 ID
 	userID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		response.BadRequest(c, "User ID invalid")
@@ -244,6 +264,7 @@ func UserPassword(c *gin.Context) {
 		return
 	}
 
+	// 验证提交数据的合法性
 	pwdHash, err := req.Validate(role.(string), authID.(int), userID)
 	if err != nil {
 		response.BadRequest(c, err.Error())
@@ -251,10 +272,12 @@ func UserPassword(c *gin.Context) {
 		return
 	}
 
+	// 执行修改密码操作
 	user := models.User{}
 	database.Connector.First(&user, userID)
 	user.Password = pwdHash
 	database.Connector.Save(&user)
 
+	// 发送响应
 	response.UserPassword(c)
 }

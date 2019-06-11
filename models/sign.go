f49752f0496a766b079a471ff84dc8c30ece3fc2
DingZhenyuan <ding_zy@outlook.com>
2019-06-11 09:25:36 +0800

    标注错误

--- models/sign.go
+++ models/sign.go
@@ -2,11 +2,11 @@ package models
 
 import "time"
 
-// Sign 请假模型
+// Sign 签到模型
 type Sign struct {
 	CommonFields
-	ShiftID uint      `json:"-"`        // 请假对应排班
-	StartAt time.Time `json:"start_at"` // 请假开始时间
-	EndAt   time.Time `json:"end_at"`   // 请假结束时间
+	ShiftID uint      `json:"-"`        // 签到对应排班
+	StartAt time.Time `json:"start_at"` // 签到开始时间
+	EndAt   time.Time `json:"end_at"`   // 签到结束时间
 	Shift   Shift     `json:"shift"`
 }

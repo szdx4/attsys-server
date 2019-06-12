87241cdc6ce9fcdebd7bbbc4eb660d4d9f62d818
kotoyuuko <kokoro@core.moe>
2019-06-13 01:45:34 +0800

    修改全员排班时间冲突判断逻辑和解决方案

--- controllers/shift.go
+++ controllers/shift.go
@@ -318,17 +318,23 @@ func ShiftAll(c *gin.Context) {
 	for _, user := range users {
 		// 处理请假冲突
 		leaveCheck := models.Shift{}
-		database.Connector.Where("user_id = ? AND start_at <= ? AND end_at >= ? AND status = pass", user.ID, endAt, startAt).First(&leaveCheck)
+		db := database.Connector.Where("user_id = ?", user.ID)
+		db = db.Where("start_at <= ? AND end_at >= ?", endAt, startAt)
+		db = db.Where("status = 'leave'")
+		db.First(&leaveCheck)
 		if leaveCheck.ID != 0 {
 			continue
 		}
 
 		// 处理排班时间冲突
 		shiftChecks := []models.Shift{}
-		database.Connector.Where("user_id = ? AND start_at <= ? AND end_at >= ?", user.ID, endAt, startAt).Find(&shiftChecks)
-		if shiftChecks != nil {
+		db = database.Connector.Where("user_id = ?", user.ID)
+		db = db.Where("start_at <= ? AND end_at >= ?", endAt, startAt)
+		db.Find(&shiftChecks)
+		startEarly := startAt
+		endLate := endAt
+		if len(shiftChecks) > 0 {
 			// 确定冲突时间段的最早最晚时间
-			var startEarly, endLate time.Time
 			for _, shiftCheck := range shiftChecks {
 				if shiftCheck.StartAt.Before(startEarly) {
 					startEarly = shiftCheck.StartAt
@@ -340,29 +346,13 @@ func ShiftAll(c *gin.Context) {
 				// 删除冲突排班
 				database.Connector.Delete(&shiftCheck)
 			}
-
-			//保留唯一排班
-			shift := models.Shift{
-				UserID:  user.ID,
-				StartAt: startEarly,
-				EndAt:   endLate,
-				Type:    req.Type,
-				Status:  "no",
-			}
-			database.Connector.Create(&shift)
-			if shift.ID == 0 {
-				response.InternalServerError(c, "Database error")
-				c.Abort()
-				return
-			}
-			continue
 		}
 
 		// 创建排班
 		shift := models.Shift{
 			UserID:  user.ID,
-			StartAt: startAt,
-			EndAt:   endAt,
+			StartAt: startEarly,
+			EndAt:   endLate,
 			Type:    req.Type,
 			Status:  "no",
 		}
